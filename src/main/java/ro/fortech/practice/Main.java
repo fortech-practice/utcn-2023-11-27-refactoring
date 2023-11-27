670db85f61373eade38847e29a2a35a0dc87d970
Dragoș Petrașcu <dragos_petrascu@yahoo.com>
2023-11-27 10:48:52 +0200

    take hourOfDay into account

--- src/main/java/ro/fortech/practice/Main.java
+++ src/main/java/ro/fortech/practice/Main.java
@@ -1,5 +1,7 @@
 package ro.fortech.practice;
 
+import java.util.Calendar;
+
 public class Main {
     public static void main(String[] args) {
         String greetingMessage = generateGreetingMessage("Yosemite Sam");
@@ -7,6 +9,25 @@ public class Main {
     }
 
     private static String generateGreetingMessage(String personName) {
-        return "Hello, " + personName + "!";
+        int hourOfDay = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+        String momentOfDay = "night";
+        if (isBetween(hourOfDay, 6, 12)) {
+            momentOfDay = "morning";
+        }
+        if (isBetween(hourOfDay, 12, 18)) {
+            momentOfDay = "afternoon";
+        }
+        if (isBetween(hourOfDay, 18, 22)) {
+            momentOfDay = "evening";
+        }
+        return "Good " + momentOfDay +  ", " + personName + "!";
+    }
+
+    private static boolean isBetween(int givenValue, int lowerLimit, int upperLimit) {
+        if (lowerLimit <= givenValue && givenValue < upperLimit) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
