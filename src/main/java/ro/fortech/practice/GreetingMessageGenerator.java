5682111775e27911d47061c2adb45edc332af4fd
Dragoș Petrașcu <dragos_petrascu@yahoo.com>
2023-11-27 11:01:01 +0200

    extract GreetingMessageGenerator

---
+++ src/main/java/ro/fortech/practice/GreetingMessageGenerator.java
@@ -0,0 +1,25 @@
+package ro.fortech.practice;
+
+import java.util.Calendar;
+
+public class GreetingMessageGenerator {
+
+    public String generateGreetingMessage(String personName) {
+        int hourOfDay = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+        String momentOfDay = "night";
+        if (isBetween(hourOfDay, 6, 12)) {
+            momentOfDay = "morning";
+        }
+        if (isBetween(hourOfDay, 12, 18)) {
+            momentOfDay = "afternoon";
+        }
+        if (isBetween(hourOfDay, 18, 22)) {
+            momentOfDay = "evening";
+        }
+        return "Good " + momentOfDay +  ", " + personName + "!";
+    }
+
+    private boolean isBetween(int givenValue, int lowerLimit, int upperLimit) {
+        return (lowerLimit <= givenValue) && (givenValue < upperLimit);
+    }
+}
--- src/main/java/ro/fortech/practice/Main.java
+++ src/main/java/ro/fortech/practice/Main.java
@@ -1,29 +1,10 @@
 package ro.fortech.practice;
 
-import java.util.Calendar;
-
 public class Main {
     public static void main(String[] args) {
-        String greetingMessage = generateGreetingMessage("Yosemite Sam");
+        String personName = "Yosemite Sam";
+        GreetingMessageGenerator generator = new GreetingMessageGenerator();
+        String greetingMessage = generator.generateGreetingMessage(personName);
         System.out.println(greetingMessage);
-    }
-
-    private static String generateGreetingMessage(String personName) {
-        int hourOfDay = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
-        String momentOfDay = "night";
-        if (isBetween(hourOfDay, 6, 12)) {
-            momentOfDay = "morning";
-        }
-        if (isBetween(hourOfDay, 12, 18)) {
-            momentOfDay = "afternoon";
-        }
-        if (isBetween(hourOfDay, 18, 22)) {
-            momentOfDay = "evening";
-        }
-        return "Good " + momentOfDay +  ", " + personName + "!";
-    }
-
-    private static boolean isBetween(int givenValue, int lowerLimit, int upperLimit) {
-        return (lowerLimit <= givenValue) && (givenValue < upperLimit);
     }
 }
